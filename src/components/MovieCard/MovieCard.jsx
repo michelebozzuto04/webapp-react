aa210769fa04f182674f87fb662ebbe16c76cd4d
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-08-01 18:31:02 +0200

    Add SingleMoviePage fetch call and review list.

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,11 +1,19 @@
+import { Link } from 'react-router-dom'
 import './MovieCard.css'
 
 function MovieCard({ movie }) {
     return (
-        <div>
-            <img src={movie.cover_img} />
-            <h3>{movie.title}</h3>
-        </div>
+        <Link to={`/movies/${movie.id}`}>
+            <div className='cardContainer'>
+                <img className='moviePoster' src={movie.image} />
+
+                {/* <div className="detailsContainer">
+                    <h3>{movie.title}</h3>
+                    <h4>{movie.director}</h4>
+                    <p>{movie.genre}</p>
+                </div> */}
+            </div>
+        </Link>
     )
 }
 
--- src/pages/SingleMoviePage/SingleMoviePage.jsx
+++ src/pages/SingleMoviePage/SingleMoviePage.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import './SingleMoviePage.css'
+import { useEffect, useState } from 'react';
 
 function SingleMoviePage() {
+
+    const { id } = useParams();
+    const singlemovie_api_url = `http://localhost:3000/api/movies/${id}`;
+    const [movie, setMovie] = useState({});
+
+    useEffect(() => {
+        fetch(singlemovie_api_url)
+            .then(res => res.json())
+            .then(data => setMovie(data))
+    }, [])
+
+    function formatDate(reviewDate) {
+        const date = new Date(reviewDate);
+
+        const day = date.getDate();
+        const month = date.getMonth();
+        const year = date.getFullYear();
+        const hour = date.getHours();
+        const minutes = date.getMinutes()
+
+        return `${day}/${month}/${year} alle ore ${hour}:${minutes}`
+    }
+
     return (
-        <div>SingleMoviePage</div>
+        <>
+            <div>
+                <h2>{movie.id}</h2>
+                <img src={movie.image} />
+            </div>
+
+            <section>
+                {movie?.reviews?.map((review) => {
+                    return (
+                        <div key={review.id} className='reviewCard'>
+                            <span>{review.name}</span>
+                            <span>{formatDate(review.created_at)}</span>
+                            <h3>{(review?.vote).toFixed(1)}</h3>
+                        </div>
+                    )
+                })}
+            </section>
+        </>
     )
 }
 
