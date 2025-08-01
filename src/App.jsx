ca15469bdd2cb5255377971a5703ad2710616068
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-08-01 17:20:28 +0200

    Add pages and header component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import MoviesPage from './pages/MoviesPage'
+import SingleMoviePage from './pages/SingleMoviePage'
+import DefaultLayout from './layouts/DefaultLayout'
 import './index.css'
 
 function App() {
 
   return (
-    <>
-      <h1>Web App</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/movies' element={<MoviesPage />} />
+          <Route path='/movies/:id' element={<SingleMoviePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,51 @@
+import { NavLink } from 'react-router-dom'
+import './Header.css'
+import logo from '../../assets/react.svg'
+import { IoSearchOutline } from 'react-icons/io5'
+
+function Header() {
+
+    const menu = [
+        {
+            id: 1,
+            title: 'Home',
+            path: '/'
+        },
+        {
+            id: 2,
+            title: 'Movies',
+            path: '/movies'
+        },
+    ]
+
+    return (
+        <header>
+            <div className="container">
+                <nav>
+                    <a href='#'>
+                        <img src={logo} />
+                    </a>
+
+                    <ul>
+                        {menu.map((menuItem) => {
+                            return (
+                                <li key={menuItem.id}>
+                                    <NavLink to={menuItem.path}>{menuItem.title}</NavLink>
+                                </li>
+                            )
+                        })}
+                    </ul>
+
+                    <form className='searchBox'>
+                        <input placeholder='Search...' />
+                        <button type='submit'>
+                            <IoSearchOutline size={22} color='rgba(255, 255, 255, 0.5)' />
+                        </button>
+                    </form>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header
